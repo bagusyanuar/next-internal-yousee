d0d9f8e5b8a6d067cfb053e801703f7d7368beaa
bagusyanuar <bagus.yanuar613@gmail.com>
2024-03-26 23:18:06 +0700

    set session user

--- app/api/login/handler.ts
+++ app/api/login/handler.ts
@@ -9,11 +9,17 @@ import { LoginRequest } from '@/model/auth'
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { cookies } from 'next/headers'
 import { getIronSession, SessionData } from 'iron-session'
-import { sessionOptions } from '@/lib/session'
+import { sessionOptions, User } from '@/lib/session'
+import { jwtDecode } from 'jwt-decode'
 
 const url: string = '/sign-in'
 type TData = {
     access_token: string
+}
+
+type TDecodedToken = {
+    username: string
+    role: string
 }
 export const LoginHandler = async (request: Request): Promise<Response> => {
     try {
@@ -25,6 +31,7 @@ export const LoginHandler = async (request: Request): Promise<Response> => {
         const response: AxiosResponse = await AxiosServer.post(url, JSON.stringify(requestBody))
         const message: string = response?.data.message
         const data: TData = response?.data.data as TData
+        const decodedToken: TDecodedToken = jwtDecode(data.access_token) as TDecodedToken
         const cookieHeaders: Array<string> = response.headers['set-cookie'] ?? []
         let cookieAuth: string = ''
         if (cookieHeaders.length > 0) {
@@ -33,12 +40,15 @@ export const LoginHandler = async (request: Request): Promise<Response> => {
         AxiosServer.defaults.headers.common.Authorization = `Bearer ${data.access_token}`
         AxiosServer.defaults.headers.common['Cookie'] = cookieAuth
 
-        console.log(cookieAuth);
-        
-        // const data: TData = response?.data.data as TData
-        // const session = await getIronSession<SessionData>(cookies(), sessionOptions)
-        // session.token = data.access_token
-        // await session.save()
+
+        const session = await getIronSession<SessionData>(cookies(), sessionOptions)
+        let sessionUser: User = {
+            username: decodedToken.username,
+            role: decodedToken.role
+        }
+        session.token = data.access_token
+        session.user = sessionUser
+        await session.save()
         return JSONSuccessResponse({ message: message })
     } catch (error: any | AxiosError) {
         let code: number = 500
@@ -47,7 +57,6 @@ export const LoginHandler = async (request: Request): Promise<Response> => {
             code = error.response ? error.response?.status : 500
             message = error.response ? error.response.data.message : 'internal server error'
             console.log(error.response?.data)
-
         }
         return JSONResponse(code, { message: message })
     }
